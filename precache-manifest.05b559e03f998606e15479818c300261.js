97cc3b6fb2c73821bbe9a30e890131b9b4ff3143
test-account-github-1 <karlevedant7@gmail.com>
2022-12-02 19:05:20 +0530

    Updates

---
+++ precache-manifest.05b559e03f998606e15479818c300261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3699dab9a5f32b19f6bf361b8b772b78",
+    "url": "/test-account-github-1/react-testing.git/index.html"
+  },
+  {
+    "revision": "2efc78ea9a8416eed277",
+    "url": "/test-account-github-1/react-testing.git/static/css/main.0c5575fa.chunk.css"
+  },
+  {
+    "revision": "72035e39894556fb4cbd",
+    "url": "/test-account-github-1/react-testing.git/static/js/2.62d1fdee.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/test-account-github-1/react-testing.git/static/js/2.62d1fdee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2efc78ea9a8416eed277",
+    "url": "/test-account-github-1/react-testing.git/static/js/main.c66d33e0.chunk.js"
+  },
+  {
+    "revision": "7d63477fc0e2530f769e",
+    "url": "/test-account-github-1/react-testing.git/static/js/runtime-main.9e40e63f.js"
+  }
+]);
